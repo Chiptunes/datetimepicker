f068aa9c182ee70d6eaa076c81d06728e8168852
Rodrigo Rojo <rrojot@hotmail.com>
2018-06-05 11:44:46 +0200

    Read only and version update

--- src/DateTimePicker.elm
+++ src/DateTimePicker.elm
@@ -52,7 +52,7 @@ import DateTimePicker.Internal exposing (InternalState(..), Time)
 import DateTimePicker.SharedStyles exposing (CssClasses(..))
 import DigitalTimePickerPanel
 import Html exposing (Html, button, div, input, li, span, table, tbody, td, text, th, thead, tr, ul)
-import Html.Attributes exposing (class, type_, value)
+import Html.Attributes exposing (class, readonly, type_, value)
 import Html.Events exposing (onBlur, onClick)
 import MultiPanel
 import Task
@@ -272,6 +272,7 @@ view pickerType attributes ((InternalState stateValue) as state) currentDate =
                         |> value
                    , type_ "text"
                    , class "input-date-picker"
+                   , readonly True
                    ]
 
         shouldForceClose config =
